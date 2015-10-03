15bf07254445f5e9de0f79a37c9a98c8b83e8f0d
wangtim1996 <wangtim1996@gmail.com>
2015-10-03 10:41:08 -0700

    Initial Android App
    Able to parse json to get list of titles.
    Cannot load images using webview

---
+++ AndroidApp/app/src/main/java/com/app/food/foodie/MainActivity.java
@@ -0,0 +1,114 @@
+package com.app.food.foodie;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+*/
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset("lunch.json"));
+            JSONArray m_jArry = obj.getJSONArray("recipes");
+            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
+            //HashMap<String, String> m_li;
+
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                Log.d("Details-->", jo_inside.getString("title"));
+        /*
+        String formula_value = jo_inside.getString("formule");
+        String url_value = jo_inside.getString("url");
+
+        //Add your values in your `ArrayList` as below:
+        m_li = new HashMap<String, String>();
+        m_li.put("formule", formula_value);
+        m_li.put("url", url_value);
+
+        formList.add(m_li);*/
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        WebView web = (WebView) findViewById(R.id.webView);
+        web.loadUrl("http://static.food2fork.com/an_ideal_lunch_saladd9cf.jpg");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String loadJSONFromAsset(String filename) {
+        String json = null;
+        try {
+
+            InputStream is = getAssets().open(filename);
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+}
