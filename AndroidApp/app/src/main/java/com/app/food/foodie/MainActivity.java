3212dd6a66bdc0ef0b5b31036012f548d32402f7
wangtim1996 <wangtim1996@gmail.com>
2015-10-03 17:55:00 -0700

    click image to open webpage... right now just opens google.com

--- AndroidApp/app/src/main/java/com/app/food/foodie/MainActivity.java
+++ AndroidApp/app/src/main/java/com/app/food/foodie/MainActivity.java
@@ -1,7 +1,9 @@
 package com.app.food.foodie;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView image;
     int windowwidth;
     int windowheight;
+    int initX;
+    int initY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         windowwidth = getWindowManager().getDefaultDisplay().getWidth();
         windowheight = getWindowManager().getDefaultDisplay().getHeight();
-        image = (ImageView) findViewById(R.id.image);
+        image = (ImageView) findViewById(R.id.imageView);
 
 
 
@@ -69,22 +73,12 @@ public class MainActivity extends AppCompatActivity {
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset("lunch.json"));
             JSONArray m_jArry = obj.getJSONArray("recipes");
-            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
-            //HashMap<String, String> m_li;
+
 
             for (int i = 0; i < m_jArry.length(); i++) {
                 JSONObject jo_inside = m_jArry.getJSONObject(i);
                 Log.d("Details-->", jo_inside.getString("title"));
-        /*
-        String formula_value = jo_inside.getString("formule");
-        String url_value = jo_inside.getString("url");
 
-        //Add your values in your `ArrayList` as below:
-        m_li = new HashMap<String, String>();
-        m_li.put("formule", formula_value);
-        m_li.put("url", url_value);
-
-        formList.add(m_li);*/
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -97,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 */
-        image = (ImageView) findViewById(R.id.imageView);
+
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -128,6 +122,8 @@ public class MainActivity extends AppCompatActivity {
                 LayoutParams layoutParams = (LayoutParams) image.getLayoutParams();
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        initX = (int) event.getRawX();
+                        initY = (int) event.getRawY();
                         break;
                     case MotionEvent.ACTION_MOVE:
                         int x_cord = (int) event.getRawX();
@@ -146,12 +142,33 @@ public class MainActivity extends AppCompatActivity {
                         image.setX(x_cord-(image.getWidth()/2));
                         image.setLayoutParams(layoutParams);
                         break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("Details-->", "ACTION UP" + initX + " "+ (int) event.getRawX() + " "+initY +" "+ (int) event.getRawY());
+
+                        if(initX == (int) event.getRawX() && initY == (int) event.getRawY())
+                        {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                            startActivity(browserIntent);
+                            Log.d("Details-->", "IMAGE CLICKED");
+                            //return false;
+                        }
+                        break;
                     default:
                         break;
                 }
                 return true;
             }
         });
+/*
+        image.setClickable(true);
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+                Log.d("Details-->", "IMAGE CLICKED");
+            }
+        });*/
+
 
 
 
